65a6772d125da92503cf1ff2bf19214437989356
Ekaterina <kattekly@mail.ru>
2022-12-20 14:27:53 +0300

    Lessons-15

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -10,14 +10,18 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
             return state
     }
 }
 
 export const setStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
 
 export type setStatusActionType = ReturnType<typeof setStatusAC>
+export type setErrorActionType = ReturnType<typeof setErrorAC>
 
-type ActionsType = setStatusActionType
+type ActionsType = setStatusActionType | setErrorActionType
 
