a81f7de9e1c36ac00627b18f8319f0c7c04ff3d5
Ekaterina <kattekly@mail.ru>
2023-01-02 12:41:04 +0300

    Lessons-16

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import './App.css'
 import {TodolistsList} from '../features/TodolistsList/TodolistsList'
 
-
-
-
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -23,25 +20,25 @@ function App() {
 
     return (
 
-            <div className="App">
-                <ErrorSnackbar/>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                            <Menu/>
-                        </IconButton>
-                        <Typography variant="h6">
-                            News
-                        </Typography>
-                        <Button color="inherit">Login</Button>
-                    </Toolbar>
-                    {status === 'loading' && <LinearProgress color="secondary"/>}
-                </AppBar>
-                <Container fixed>
-                        <TodolistsList/>
-                        <Login/>
-                </Container>
-            </div>
+        <div className="App">
+            <ErrorSnackbar/>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+                {status === 'loading' && <LinearProgress color="secondary"/>}
+            </AppBar>
+            <Container fixed>
+                <TodolistsList/>
+                <Login/>
+            </Container>
+        </div>
 
     )
 }
