34852b69f058a7c0fda509143b43139e150faa08
Ekaterina <kattekly@mail.ru>
2023-02-02 11:11:34 +0300

    Lesson-17

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {combineReducers} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from '../features/Login/auth-reducer'
@@ -15,11 +15,12 @@ const rootReducer = combineReducers({
     auth: authReducer
 })
 // непосредственно создаём store
-/*export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));*/
+/*export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));*/ //Redux
+
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware)
-})
+}) //Redux Toolkit
 
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
