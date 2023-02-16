595a25e50000e9276489c60e0c9455a6341ac4cc
Ekaterina <kattekly@mail.ru>
2023-02-16 14:32:48 +0300

    Lesson-18

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -13,9 +13,15 @@ const slice = createSlice({
     name: 'app',
     initialState: initialState,
     reducers: {
-        setAppStatusAC(state, action: PayloadAction<{status: RequestStatusType}>) {state.status = action.payload.status},
-        setAppErrorAC(state, action: PayloadAction<{error: string | null}>) {state.error = action.payload.error},
-        setAppInitializedAC(state, action: PayloadAction<{ isInitialized: boolean }>) {state.isInitialized = action.payload.isInitialized}
+        setAppStatusAC(state, action: PayloadAction<{ status: RequestStatusType }>) {
+            state.status = action.payload.status
+        },
+        setAppErrorAC(state, action: PayloadAction<{ error: string | null }>) {
+            state.error = action.payload.error
+        },
+        setAppInitializedAC(state, action: PayloadAction<{ isInitialized: boolean }>) {
+            state.isInitialized = action.payload.isInitialized
+        }
     }
 })
 
