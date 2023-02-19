2420a85b8b1ce261481e7a9db430aeb26b2a5030
Ekaterina <kattekly@mail.ru>
2023-02-19 17:17:31 +0300

    Lesson-19

--- src/features/TodolistsList/todolists-tasks-reducer.test.ts
+++ src/features/TodolistsList/todolists-tasks-reducer.test.ts
@@ -17,7 +17,6 @@ test('ids should be equals', () => {
 
     const endTasksState = tasksReducer(startTasksState, action)
     const endTodolistsState = todolistsReducer(startTodolistsState, action)
-
     const keys = Object.keys(endTasksState);
     const idFromTasks = keys[0];
     const idFromTodolists = endTodolistsState[0].id;
