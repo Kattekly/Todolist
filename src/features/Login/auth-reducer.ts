e130dd0dd75b38df72a6be977ca82cbd6c71a7f5
Ekaterina <kattekly@mail.ru>
2023-02-22 16:28:54 +0300

    Lesson-19

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -4,20 +4,19 @@ import {authAPI, LoginParamsType} from '../../api/todolists-api'
 import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
 import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 
-export const loginTC = createAsyncThunk('auth/login', (data: LoginParamsType, thunkAPI) => {
+export const loginTC = createAsyncThunk('auth/login', async (data: LoginParamsType, thunkAPI) => {
     thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
-    authAPI.login(data)
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                thunkAPI.dispatch(setIsLoggedInAC({value: true}))
-                thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
-            } else {
-                handleServerAppError(res.data, thunkAPI.dispatch)
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkError(error, thunkAPI.dispatch)
-        })
+    try {
+        const res = await authAPI.login(data)
+        if (res.data.resultCode === 0) {
+            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+            return {isLoggedIn: true}
+        } else {
+            handleServerAppError(res.data, thunkAPI.dispatch)
+        }
+    } catch (error) {
+        handleServerNetworkError(error, thunkAPI.dispatch)
+    }
 })
 
 const slice = createSlice({
