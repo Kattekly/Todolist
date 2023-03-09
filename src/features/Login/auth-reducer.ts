998e8eb4fee3c063c6f9ab0008dcad5f53a408c4
Ekaterina <kattekly@mail.ru>
2023-03-09 15:20:16 +0300

    Lesson 17

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -2,27 +2,26 @@ import {Dispatch} from 'redux'
 import {setAppStatusAC} from '../../app/app-reducer'
 import {authAPI, LoginParamsType} from '../../api/todolists-api'
 import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
-import {createSlice, PayloadAction} from '@reduxjs/toolkit'
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
 
-const initialState = {
-    isLoggedIn: false
-}
-
-const slice = createSlice({
-    name: 'auth',
-    initialState: initialState,
-    reducers: {
-        setIsLoggedInAC(state, action: PayloadAction<{value: boolean}>) {
-            state.isLoggedIn = action.payload.value
+export const loginTC = createAsyncThunk('task/login', async (data: LoginParamsType, thunkAPI) => {
+    thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
+    const res = await authAPI.login(data)
+    try {
+        if (res.data.resultCode === 0) {
+            thunkAPI.dispatch(setIsLoggedInAC({value: true}))
+            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+        } else {
+            handleServerAppError(res.data, thunkAPI.dispatch)
         }
+    } catch {
+        /*  ((error) => {
+                  handleServerNetworkError(error, thunkAPI.dispatch)
+              })*/
     }
 })
 
-export const authReducer = slice.reducer
-export const {setIsLoggedInAC} = slice.actions // вместо АС
-
-// thunks
-export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
+export const loginTC_ = (data: LoginParamsType) => (dispatch: Dispatch) => {
     dispatch(setAppStatusAC({status: 'loading'}))
     authAPI.login(data)
         .then(res => {
@@ -37,6 +36,23 @@ export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
             handleServerNetworkError(error, dispatch)
         })
 }
+
+const slice = createSlice({
+    name: 'auth',
+    initialState: {
+        isLoggedIn: false
+    },
+    reducers: {
+        setIsLoggedInAC(state, action: PayloadAction<{ value: boolean }>) {
+            state.isLoggedIn = action.payload.value
+        }
+    }
+})
+
+export const authReducer = slice.reducer
+export const {setIsLoggedInAC} = slice.actions // вместо АС
+
+// thunks
 
 export const logoutTC = () => (dispatch: Dispatch) => {
     dispatch(setAppStatusAC({status: 'loading'}))
