0785e62b384ef3482c8ce69134b64b9307402043
Ekaterina <kattekly@mail.ru>
2023-03-10 13:43:55 +0300

    Lesson 18

--- src/features/TodolistsList/tasks-reducer.test.ts
+++ src/features/TodolistsList/tasks-reducer.test.ts
@@ -1,4 +1,4 @@
-import {addTaskTC, fetchTasksTC, removeTaskTC, tasksReducer, TasksStateType, updateTaskAC} from './tasks-reducer'
+import {addTaskTC, fetchTasksTC, removeTaskTC, tasksReducer, TasksStateType, updateTaskTC} from './tasks-reducer'
 import {addTodolistAC, removeTodolistAC, setTodolistsAC} from './todolists-reducer'
 import {TaskPriorities, TaskStatuses} from '../../api/todolists-api'
 
@@ -73,7 +73,7 @@ test('correct task should be added to correct array', () => {
     expect(endState['todolistId2'][0].status).toBe(TaskStatuses.New)
 })
 test('status of specified task should be changed', () => {
-    const action = updateTaskAC({taskId: '2', model: {status: TaskStatuses.New}, todolistId: 'todolistId2'})
+    const action = updateTaskTC.fulfilled({taskId: '2', model: {status: TaskStatuses.New}, todolistId: 'todolistId2'}, '', {taskId: '2', model: {status: TaskStatuses.New}, todolistId: 'todolistId2'})
 
     const endState = tasksReducer(startState, action)
 
@@ -81,7 +81,7 @@ test('status of specified task should be changed', () => {
     expect(endState['todolistId2'][1].status).toBe(TaskStatuses.New)
 })
 test('title of specified task should be changed', () => {
-    const action = updateTaskAC({taskId: '2', model: {title: 'yogurt'}, todolistId: 'todolistId2'})
+    const action = updateTaskTC.fulfilled({taskId: '2', model: {title: 'yogurt'}, todolistId: 'todolistId2'}, '', {taskId: '2', model: {title: 'yogurt'}, todolistId: 'todolistId2'})
 
     const endState = tasksReducer(startState, action)
 
