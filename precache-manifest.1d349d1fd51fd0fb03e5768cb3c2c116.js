73093369c2c0272d9cdcfa46f076f8c48c100af0
Ekaterina <kattekly@mail.ru>
2023-03-17 12:40:10 +0300

    Updates

---
+++ precache-manifest.1d349d1fd51fd0fb03e5768cb3c2c116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc0b18c2ac5508cf17d14e44c2759d8",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "c3c31f651120dd29f7ba",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "f8edfdd30cfe1adf67cc",
+    "url": "/Todolist/static/js/2.07ede5bd.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.07ede5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c31f651120dd29f7ba",
+    "url": "/Todolist/static/js/main.4f5594a5.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
