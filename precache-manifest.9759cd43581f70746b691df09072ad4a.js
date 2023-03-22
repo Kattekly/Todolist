e8912421e8b6612711854b4aee1fd45c9e59fe37
Ekaterina <kattekly@mail.ru>
2023-03-22 12:24:10 +0300

    Updates

---
+++ precache-manifest.9759cd43581f70746b691df09072ad4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2688c90310091b219e42a05ceca8f98",
+    "url": "/it-incubator-Todolist/index.html"
+  },
+  {
+    "revision": "d7f09e837ab802fad15f",
+    "url": "/it-incubator-Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "f8edfdd30cfe1adf67cc",
+    "url": "/it-incubator-Todolist/static/js/2.07ede5bd.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/it-incubator-Todolist/static/js/2.07ede5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f09e837ab802fad15f",
+    "url": "/it-incubator-Todolist/static/js/main.d5f2e9ff.chunk.js"
+  },
+  {
+    "revision": "f1023609ff6f7e5b55a1",
+    "url": "/it-incubator-Todolist/static/js/runtime-main.06eef9af.js"
+  }
+]);
